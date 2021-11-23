0d8c8e183f0acc9ef3c4b329b1004cd9bb299a73
Field Employee <fieldemployee@Admins-MacBook-Pro.local>
2021-11-22 19:16:26 -0500

    Converted BuildConfig file to kotlin

---
+++ app/build/generated/source/buildConfig/debug/com/inaki/lifecycleapp/BuildConfig.kt
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.inaki.lifecycleapp
+
+import java.lang.Boolean
+
+object BuildConfig {
+    val DEBUG = Boolean.parseBoolean("true")
+    const val APPLICATION_ID = "com.inaki.lifecycleapp"
+    const val BUILD_TYPE = "debug"
+    const val VERSION_CODE = 1
+    const val VERSION_NAME = "1.0"
+}
